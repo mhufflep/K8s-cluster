3a4791a97672b0e2f6705e1cd86b100dbef9ff67
mhufflep <mhufflep@student.21-school.ru>
2021-06-19 20:00:43 +0300

    last update

--- srcs/delete.sh
+++ srcs/delete.sh
@@ -2,3 +2,5 @@ docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker container prune
 docker rmi $(docker images -q)
+
+# docker image rm $(docker images | grep grafana_img | tr -s ' ' | cut -d" " -f3)
